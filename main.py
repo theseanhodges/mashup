bb11b4e7090227aa52558f1d27631423fca9f6ca
Sean Hodges <sthodges@gmail.com>
2021-03-16 16:27:35 -0700

    Put fact into Flask

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import os
+import base64
+import time
+
+from flask import Flask, render_template_string, request, redirect, url_for
+
 from bs4 import BeautifulSoup
 import requests
 
-html = requests.get('http://unkno.com/')
-parser = BeautifulSoup(html.content, 'html.parser')
+app = Flask(__name__)
 
-quote = parser.find('div', id='content').getText().strip()
+@app.route('/')
+def home():
+    html = requests.get('http://unkno.com/')
+    parser = BeautifulSoup(html.content, 'html.parser')
+
+    fact = parser.find('div', id='content').getText().strip()
+
+    return render_template_string(fact)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
